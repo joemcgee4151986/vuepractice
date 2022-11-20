ed7940dded53adc7742c7c8997d928880e92192b
joe mcgee <jrmcgee@asu.edu>
2022-11-20 23:55:48 +0900

    updating app and adding style.css

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
-    template: '<h1>Hello world</h1>',
+    template: '<h1>Hello {{firstName}}!</h1>',
+    data(){
+        return {
+            firstName: 'Joe',
+        }
+    },
 })
 
 app.mount('#app')
