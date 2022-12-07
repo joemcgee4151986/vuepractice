6b4afaff69cdc7de9c72f2812e615ba1405869f7
joe mcgee <jrmcgee@asu.edu>
2022-12-08 00:49:07 +0900

    updated crash course project with new code

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const app = Vue.createApp({
     data(){
         return {
             firstName: 'Joe',
+            lastName: 'McGee',
+            email: 'fake@fakeemail.com',
+            gender: 'male',
+            picture: 'http://clipart-library.com/newimages/man-clipart-3.jpg',
         }
     },
+    methods: {
+            async getUser() {
+                const res = await fetch('https://randomuser.me/api')
+                const { results } = await res.json()
+                console.log(results)
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+            console.log(this.firstName);
+        }
+    }
 })
 
 app.mount('#app')
